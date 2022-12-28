f3e19888845a69825f20d8ff7220f344c0bfed3a
Brandon Chin <bbrandonchin@yahoo.com>
2022-12-28 04:22:35 +0000

    Added submit event listener to create object of input values and reset form when save button is pressed

--- js/main.js
+++ js/main.js
@@ -2,5 +2,24 @@ var $imageInput = document.querySelector('#photo-url');
 var $imageSource = document.querySelector('img');
 $imageInput.addEventListener('input', image);
 function image(event) {
-  $imageSource.src = event.target.value;
+  // $imageSource.src = event.target.value;
+  $imageSource.setAttribute('src', event.target.value);
+}
+
+var $journalForm = document.querySelector('#journal-form');
+$journalForm.addEventListener('submit', form);
+var nextEntryId = [];
+function form(Event) {
+
+  event.preventDefault();
+
+  var entryId =
+    {
+      title: document.querySelector('#title').value,
+      photoUrl: document.querySelector('#photo-url').value,
+      Notes: document.querySelector('#text-area').value
+    };
+  $imageSource.setAttribute('src', 'images/placeholder-image-square.jpg');
+  nextEntryId.push(entryId);
+  $journalForm.reset();
 }
