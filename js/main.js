fc5f2bbfc9d560b30a8cbfdb861ed6a64856598c
Brandon Chin <bbrandonchin@yahoo.com>
2023-01-03 01:22:05 +0000

    added event listener to edit entry when pencil icon is clicked.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var $imageInput = document.querySelector('#photo-url');
 var $imageSource = document.querySelector('img');
 $imageInput.addEventListener('input', image);
+
 function image(event) {
   $imageSource.setAttribute('src', event.target.value);
 }
@@ -23,7 +24,7 @@ function form(Event) {
 
   data.entries.unshift(inputValues);
 
-  viewSwap('entries');
+  viewSwap('entry-form');
   toggleNoEntries();
 
   $imageSource.setAttribute('src', 'images/placeholder-image-square.jpg');
@@ -35,6 +36,7 @@ var ul = document.querySelector('ul');
 function renderEntry(entry) {
   var li = document.createElement('li');
   li.setAttribute('class', 'row');
+  li.setAttribute('data-entry-id', entry.entryId);
   var div = document.createElement('div');
   div.setAttribute('class', 'column-half');
   li.appendChild(div);
@@ -47,10 +49,16 @@ function renderEntry(entry) {
   var h2 = document.createElement('h2');
   h2.appendChild(document.createTextNode(entry.title));
   secondDiv.appendChild(h2);
+  var anchor = document.createElement('a');
+  anchor.setAttribute('href', '#');
+  anchor.setAttribute('id', 'edit');
+  h2.appendChild(anchor);
+  var fontAwesome = document.createElement('i');
+  fontAwesome.setAttribute('class', 'fa fa-pencil');
+  anchor.appendChild(fontAwesome);
   var paragraph = document.createElement('p');
   paragraph.appendChild(document.createTextNode(entry.notes));
   secondDiv.appendChild(paragraph);
-
   return li;
 }
 
@@ -80,6 +88,8 @@ var $buttonToggleHeadingA = document.querySelector('#heading-a');
 $buttonToggleHeadingA.addEventListener('click', click);
 var $buttonToggleNew = document.querySelector('#new');
 $buttonToggleNew.addEventListener('click', click);
+var $editButton = document.querySelector('ul');
+$editButton.addEventListener('click', click);
 
 function viewSwap(viewtype) {
   data.view = viewtype;
@@ -103,5 +113,19 @@ function click(event) {
   if (event.target.matches('#new')) {
     viewSwap('entry-form');
     $h2.textContent = 'New Entry';
+  }
+  if (event.target.closest('ul')) {
+    viewSwap('entry-form');
+    $h2.textContent = 'Edit Entry';
+    var currentEntryId = event.target.closest('li');
+    currentEntryId = Number(currentEntryId.getAttribute('data-entry-id'));
+    for (let i = 0; i < data.entries.length; i++) {
+      if (data.entries[i].entryId === currentEntryId) {
+        data.editing = data.entries[i];
+        $journalForm.elements.title.value = data.editing.title;
+        $journalForm.elements.photoUrl.value = data.editing.photoUrl;
+        $journalForm.elements.notes.value = data.editing.notes;
+      }
+    }
   }
 }
